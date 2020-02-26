8e7df609a3375097751f821d40a08c85724ba1c5
alepeteporico <tojandro@gmail.com>
2020-02-26 18:19:00 +0100

    Fichero del ejercicio principal añadido

---
+++ ejercicio.py
@@ -0,0 +1,5 @@
+while True:
+    print("=============================================MENÚ=============================================")
+    print("1. Listar consolas de sobremesa o portatiles y después te pregunta si quieres listar los juegos de alguna de ellas.")
+    print("2. Contar los juegos de una consola en total y también los que son exclusivos.")
+    print("==============================================================================================")
